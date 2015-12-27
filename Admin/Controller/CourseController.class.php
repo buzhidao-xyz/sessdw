2b39f43320905e5b7020b3debab14a030978c826
imbzd <luochuan1234>
2015-12-27 23:24:47 +0800

    课程功能模块开发中

--- Admin/Controller/CourseController.class.php
+++ Admin/Controller/CourseController.class.php
@@ -8,6 +8,13 @@ namespace Admin\Controller;
 
 class CourseController extends CommonController
 {
+    // 课程分类
+    private $_course_class = array(
+        1 => array('id'=>1, 'name'=>'学党章'),
+        2 => array('id'=>2, 'name'=>'学讲话'),
+        3 => array('id'=>3, 'name'=>'学条例'),
+    );
+
     public function __construct()
     {
         parent::__construct();
@@ -15,10 +22,94 @@ class CourseController extends CommonController
         $this->assign("sidebar_active", array("Course","index"));
 
         $this->_page_location = __APP__.'?s=Course/index';
+
+        $this->assign("classlist", $this->_course_class);
     }
 
+    //获取课程id
+    private function _getCourseid()
+    {
+        $courseid = mRequest('courseid');
+
+        return $courseid;
+    }
+
+    //获取课程标题
+    private function _getTitle()
+    {
+        $title = mRequest('title');
+        if (!$title) $this->ajaxReturn(1, '请填写课程标题！');
+
+        return $title;
+    }
+
+    //获取课程分类
+    private function _getClassid()
+    {
+        $classid = mRequest('classid');
+        if (!$classid) $this->ajaxReturn(1, '请选择课程分类！');
+
+        return $classid;
+    }
+
+    //获取搜索关键字
+    private function _getKeywords()
+    {
+        $keywords = mRequest('keywords');
+        $this->assign('keywords', $keywords);
+
+        return $keywords;
+    }
+
+    //课程管理
     public function index()
     {
+        $keywords = $this->_getKeywords();
+
+        list($start, $length) = $this->_mkPage();
+        $data = D('Course')->getCourse(null, null, $keywords, $start, $length);
+        $total = $data['total'];
+        $datalist = $data['data'];
+
+        $this->assign('datalist', $datalist);
+
+        $param = array(
+            'keywords'   => $keywords,
+        );
+        $this->assign('param', $param);
+        //解析分页数据
+        $this->_mkPagination($total, $param);
+
         $this->display();
+    }
+
+    //上传课程示例图
+    public function showimgupload()
+    {
+        
+    }
+    
+    //上传视频封面图
+    public function videoimgupload()
+    {
+        
+    }
+
+    //发布课程
+    public function newcourse()
+    {
+        $this->display();
+    }
+
+    //编辑课程
+    public function upcourse()
+    {
+        $this->display();
+    }
+
+    //保存课程
+    public function coursesave()
+    {
+
     }
 }
---
+++ Admin/Model/CourseModel.class.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * 课程数据模型
+ * 2015-12-22
+ * buzhidao
+ */
+namespace Admin\Model;
+
+class CourseModel extends CommonModel
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    //获取课程
+    public function getCourse($courseid=null, $classid=null, $keyword=null, $start=0, $length=9999)
+    {
+        $where = array();
+        if ($courseid) $where['courseid'] = $courseid;
+        if ($classid) $where['classid'] = $classid;
+        if ($keyword) $where['title'] = array('like', '%'.$keyword.'%');
+
+        $count = M('course')->where($where)->count();
+        $result = M('course')->where($where)->order('createtime desc')->limit($start, $length)->select();
+
+        return array('total'=>$count, 'data'=>is_array($result)?$result:array());
+    }
+
+    //获取课程详情
+    public function getCourseByID($courseid=null)
+    {
+        if (!$courseid) return false;
+
+        $datainfo = $this->getCourse($courseid);
+
+        return $datainfo['total'] ? $datainfo['data'][0] : array();
+    }
+
+    //保存课程
+    public function saveCourse($courseid=null, $data=array())
+    {
+        if (!is_array($data) || empty($data)) return false;
+
+        if ($courseid) {
+            $result = M('course')->where(array('courseid'=>$courseid))->save($data);
+            $result = $result ? $courseid : false;
+        } else {
+            $courseid = M('course')->add($data);
+        }
+
+        return $courseid ? $courseid : false;
+    }
+}
