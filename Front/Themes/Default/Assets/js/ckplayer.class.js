3d2a41948b46c9e9c103a6082802fe3e60b515d8
imbzd <luochuan1234>
2015-12-30 22:35:49 +0800

    视频封面修改

--- Front/Themes/Default/Assets/js/ckplayer.class.js
+++ Front/Themes/Default/Assets/js/ckplayer.class.js
@@ -45,7 +45,7 @@ var flashvars={
     c:1,
     x:'ckplayer.xml',
     p:0,
-    i:HOST_PATH+'Upload/course/cover/course_test_cover.png',
+    i:HOST_PATH+'Upload/course/cover/学党章_1207.png',
     wh:'16:9',
     e:6,
     b:0,
